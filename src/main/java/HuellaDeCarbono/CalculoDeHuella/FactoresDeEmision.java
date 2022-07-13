01527666c795449cb57ea6d11f0b157b4dffb184
gonza <gonzalorduarte@gmail.com>
2022-07-13 09:33:49 -0300

    nuevo singleton

--- src/main/java/HuellaDeCarbono/CalculoDeHuella/FactoresDeEmision.java
+++ src/main/java/HuellaDeCarbono/CalculoDeHuella/FactoresDeEmision.java
@@ -4,6 +4,28 @@ import java.util.HashMap;
 
 public class FactoresDeEmision {
     private HashMap<String, Double> FE = new HashMap<String, Double>();
+    /*
+    public final class Singleton {
+        private static Singleton instance;
+        public String value;
+
+        private Singleton(String value) {
+            // The following code emulates slow initialization.
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            this.value = value;
+        }
+
+        public static Singleton getInstance(String value) {
+            if (instance == null) {
+                instance = new Singleton(value);
+            }
+            return instance;
+        }
+    }*/
     private static FactoresDeEmision instance = new FactoresDeEmision();
 
     public static FactoresDeEmision getFactores() {
